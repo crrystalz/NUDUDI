ddf735689c3051c422bc93498f6797803e89a6ae
crrystalz <lichupatnaik@gmail.com>
2023-02-20 12:52:42 -0800

    proccess dataset OVERHAUL (see description)
    Fixed bugs, added functionality to choose how many from each anomaly, and most importantly PROGRESS BAR!

--- proccess_dataset.py
+++ proccess_dataset.py
@@ -5,11 +5,14 @@ import shutil
 import cv2
 import matplotlib.pyplot as plt
 
+from utils import printProgressBar
+
 # Type 1
 
 
 def combine_files(directory):
     for field_id in os.listdir(directory):
+        print("New image: " + field_id)
         combine_files_for_one_field(directory, field_id)
 
 
@@ -56,7 +59,7 @@ def is_black(image, tolerance=10):
     return np.all(np.abs(image - 0) <= tolerance)
 
 
-def create_dataset(path_to_train_dir, output_dataset_dir):
+def create_dataset(path_to_train_dir, output_dataset_dir, num_anomalies, images_per_anomaly = -1):
     for root, dirs, files in os.walk(output_dataset_dir):
         for f in files:
             os.unlink(os.path.join(root, f))
@@ -68,8 +71,25 @@ def create_dataset(path_to_train_dir, output_dataset_dir):
     images_dir = os.path.join(path_to_train_dir, "images", "rgb")
     labels_dir = os.path.join(path_to_train_dir, "labels")
 
+    rgb_images_path = path_to_train_dir + "\\images\\rgb"
+    total_files = sum([len(files) for _, _, files in os.walk(rgb_images_path)])
+    num_files = 0
+
+    if images_per_anomaly == -1:
+        images_per_anomaly = total_files
+        num_files = total_files * num_anomalies
+    else:
+        num_files = images_per_anomaly * num_anomalies
+
+    print()
+    printProgressBar(0, num_files, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    progress = 0
+
+    fig, ax = plt.subplots()
+
     for anomaly_type in os.listdir(labels_dir):
         anomaly_dir = os.path.join(labels_dir, anomaly_type)
+        anomaly_dir_files = os.listdir(anomaly_dir)
 
         # For each anomaly type, create a directory stucture
         # like the following inside output_dataset_dir.
@@ -91,7 +111,10 @@ def create_dataset(path_to_train_dir, output_dataset_dir):
             os.mkdir(output_annotated_image_dir)
 
         count = 0
-        for file_name in os.listdir(anomaly_dir):
+        i = 0
+        while count != images_per_anomaly:
+            file_name = anomaly_dir_files[i]
+
             image = cv2.imread(f"{anomaly_dir}\\{file_name}", cv2.IMREAD_GRAYSCALE)
             if not is_black(image):
                 if os.path.exists(f"{images_dir}\\{file_name}"):
@@ -107,16 +130,23 @@ def create_dataset(path_to_train_dir, output_dataset_dir):
                 anomalous_image_bw = Image.open(f"{anomaly_dir}\\{file_name}").convert('L')
                 anomalous_image_rgb = Image.open(f"{output_image_dir}\\{file_name}")
                 overlay_anomalous_region_boundary(
-                    anomalous_image_bw, anomalous_image_rgb, f"{output_annotated_image_dir}/{file_name}"
+                    anomalous_image_bw, anomalous_image_rgb, f"{output_annotated_image_dir}/{file_name}", fig, ax
                 )
+
                 count = count + 1
-        print(anomaly_type + " count: " + str(count))
+                progress += 1
+
+                if progress < num_files: printProgressBar(progress + 1, num_files, prefix = 'Progress:', suffix = 'Complete', length = 50)
+            
+            i += 1
+        
+        plt.close(fig)
+
+        # print(anomaly_type + " count: " + str(count))
 
 
-#
 # returns a list (x,y) corrdinates that define the boundary of
 # anomalous region.
-#
 def get_anomaly_outline(anomalous_image):
     outline_x = []
     outline_y = []
@@ -134,22 +164,19 @@ def get_anomaly_outline(anomalous_image):
     return outline_x, outline_y
 
 
-#
 # Overlays the anomalous region outline on the image.
-#
-def overlay_anomalous_region_boundary(anomalous_image_bw, anomalous_image_rgb, annotated_image_file_path):
+def overlay_anomalous_region_boundary(anomalous_image_bw, anomalous_image_rgb, annotated_image_file_path, fig, ax):
     x, y = get_anomaly_outline(anomalous_image_bw)
     img_array = np.array(anomalous_image_rgb)
-    plt.figure()
-    fig, ax = plt.subplots()
+
     ax.imshow(img_array)
     ax.scatter(x, y, s = 1)
     plt.savefig(annotated_image_file_path)
-    plt.close()
+    plt.clf()
 
 
-# combine_files("E:/nududi_datasets/testing2")
+# combine_files("F:/nududi_datasets/testing-2")
 
-train_dir = "E:\\nududi_datasets\\Agriculture-Vision-2021\\train"
-output_dataset_dir = "E:\\nududi_datasets\\testing4"
-create_dataset(train_dir, output_dataset_dir)
+train_dir = "F:\\nududi_datasets\\agriculture-vision-2021-supervised\\supervised\\Agriculture-Vision-2021\\train"
+output_dataset_dir = "F:\\nududi_datasets\\testing-1"
+create_dataset(train_dir, output_dataset_dir, 9, 5)
